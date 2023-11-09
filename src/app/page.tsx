c5f4eb7591eab0fb9c90c844283f639db05e108a
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2023-11-09 18:17:14 +0100

    added searchbar and logo to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,43 @@
 import { api } from "~/trpc/server";
-import RecipeCard from "~/app/_components/RecipeCard";
+import RecipeCard from "~/app/_components/RecipeCard"
+import { Input } from '@nextui-org/react';
 
 export default async function Home() {
   const featuredRecipes = await api.recipe.getFeaturedRecipes.query({
     take: 6,
   });
 
-  return (
-    <main className="flex justify-center">
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-        {featuredRecipes ? (
-          featuredRecipes.map((recipe) => (
-            <RecipeCard recipeId={recipe.id} key={recipe.id} />
-          ))
-        ) : (
-          <h2>No recipes found...</h2>
-        )}
-      </div>
-    </main>
-  );
+    return (
+        <main className="flex flex-col items-center">
+            <div className="mb-4">
+                {/* Goose chef logo */}
+                <img
+                    src="/images/Logo_round_V2.png"
+                    alt="Logo"
+                    className="w-24 h-24 mb-2 object-contain"
+                />
+            </div>
+            <div className="w-full md:w-1/2 mb-4">
+                <Input
+                    type="text"
+                    //value={searchQuery}
+                    //onChange={(e) => setSearchQuery(e.target.value)}
+                    //onKeyPress={handleKeyPress}
+                    placeholder="Search recipes..."
+                    //bordered
+                    //size="large"
+                    fullWidth
+                />
+            </div>
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+                {featuredRecipes ? (
+                    featuredRecipes.map((recipe) => (
+                        <RecipeCard recipeId={recipe.id} key={recipe.id} />
+                    ))
+                ) : (
+                    <h2>No recipes found...</h2>
+                )}
+            </div>
+        </main>
+    );
 }
