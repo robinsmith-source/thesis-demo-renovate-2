a8d15be611726118e348089c5a6fea394c47d3da
Robin Schmidt <schmidtrobin02@gmail.com>
2023-12-20 21:23:22 +0100

    fix: use correct method

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,6 +17,6 @@ export default async function middleware(req: NextRequest, res: NextResponse) {
     req.nextUrl.pathname.match(/^\/recipe\/[^\/]+\/edit/) &&
     !isAuthenticated
   ) {
-    return NextResponse.rewrite(new URL("/auth/signin", req.url));
+    return NextResponse.redirect(new URL("/auth/signin", req.url));
   }
 }
