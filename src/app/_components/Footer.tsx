1b06eb850a56365bd9f771c2ed80695f57e81af4
KuroiKoneko <75211512+KuroiKoneko@users.noreply.github.com>
2024-01-19 16:45:24 +0100

    fix: changed appearance of footer

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -4,37 +4,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="m-4 rounded-lg bg-white shadow dark:bg-gray-800">
-      <div className="mx-auto w-full max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2023{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
-            Flowbite™
-          </a>
-          . All Rights Reserved.
-        </span>
-        <ul className="mt-3 flex flex-wrap items-center text-sm font-medium text-gray-500 sm:mt-0 dark:text-gray-400">
-          <li>
-            <a href="#" className="me-4 hover:underline md:me-6">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="#" className="me-4 hover:underline md:me-6">
-              Privacy Policy
-            </a>
-          </li>
-          <li>
-            <a href="#" className="me-4 hover:underline md:me-6">
-              Licensing
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:underline">
-              Contact
-            </a>
-          </li>
-        </ul>
+    <footer className=" bg-primary-400">
+      <div className="mx-auto flex w-full max-w-screen-xl justify-center p-4">
+        <p className="text-lg text-black">© 2023 Goose Chef</p>
       </div>
     </footer>
   );
